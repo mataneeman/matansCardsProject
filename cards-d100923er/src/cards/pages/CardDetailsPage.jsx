51c362dae5d8ff7d354dc8d99081275ad1ad9b8c
mataneeman <155008652+mataneeman@users.noreply.github.com>
2024-05-22 17:12:15 +0300

    collapse detailes

--- cards-d100923er/src/cards/pages/CardDetailsPage.jsx
+++ cards-d100923er/src/cards/pages/CardDetailsPage.jsx
@@ -1,5 +1,5 @@
-import { Avatar, Box, Container,Paper,Typography } from '@mui/material'
-import React, { useEffect } from 'react'
+import { Avatar, Box, Collapse, Container,IconButton,Paper,Typography, styled } from '@mui/material'
+import React, { useEffect, useState } from 'react'
 import PageHeader from '../../components/PageHeader'
 import useCards from '../hooks/useCards'
 import { useParams } from "react-router-dom";
@@ -8,11 +8,28 @@ import Spinner from '../../components/Spinner';
 import Error from '../../components/Error';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import { useTheme } from '../../providers/CustomThemeProvider';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 export default function CardDetailsPage() {
   const{getCardById,card,isLoading,error}=useCards()
   const{id}=useParams()
   const {isDark} =useTheme()
+  const [expanded, setExpanded] =useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
  
   useEffect(()=>{
     getCardById(id)
@@ -51,13 +68,34 @@ export default function CardDetailsPage() {
               <Paper elevation={5} sx={{mt:2,p:2,mb:2,width:"85%"}}>       
                 <Typography mt={1}><strong>Title:</strong> {card.title} </Typography>
                 <Typography mt={1}><strong>Subtitle:</strong> {card.subtitle} </Typography>
-                <Typography mt={1}><strong>BizNumber:</strong> {card.bizNumber} </Typography>
-                <Typography mt={1}><strong>Description:</strong> {card.description} </Typography>
                 <Typography mt={1}><strong>Email:</strong> {card.email}</Typography>
                 <Typography mt={1}><strong>Phone: </strong> {card.phone} </Typography>
-                <Typography mt={1}><strong>User Id:</strong> {card.user_id} </Typography>
-                <Typography mt={1}><strong>CreateAT:</strong> {card.createdAt} </Typography>
-                <Typography mt={1} mb={2} sx={{border:1,borderRadius:2,p:1,width:50}}>{card.likes.length} <FavoriteIcon color='error'/></Typography>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                  <Typography mt={1}><strong>BizNumber:</strong> {card.bizNumber} </Typography>
+                  <Typography mt={1}><strong>Description:</strong> {card.description} </Typography>
+                  <Typography mt={1}><strong>User Id:</strong> {card.user_id} </Typography>
+                  <Typography mt={1}><strong>CreateAT:</strong> {card.createdAt} </Typography>
+                  <Typography 
+                    sx={{
+                      mt:1,
+                      border:1,
+                      borderRadius:2,
+                      p:1,
+                      width:100,
+                      display:'flex',
+                      alignItems:"center",
+                      justifyContent:'space-around'}}>
+                      <FavoriteIcon color='error'/>{card.likes.length} Likes</Typography>
+                </Collapse>
+                <ExpandMore
+                  expand={expanded}
+                  onClick={handleExpandClick}
+                  aria-expanded={expanded}
+                  aria-label="show more"
+                  title="more info"
+                  >
+                 <ExpandMoreIcon />
+                </ExpandMore>
               </Paper>  
           </Box>
 
@@ -65,7 +103,7 @@ export default function CardDetailsPage() {
           <Box 
             sx={{ 
             width: {xs:"100%",md:"50%"},
-            height:{xs:"100%",md:"400px"},
+            height:{xs:"100%",md:"350px"},
             }}>
               <Typography mb={2} textAlign='start' fontSize={20}><strong>Address Card:</strong></Typography>
               <Box  
